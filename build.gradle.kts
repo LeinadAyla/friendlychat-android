2171b2bf2f0dfde96f7163652bb67a4165d1b297
Wilson <wilsonfreitashgem@gmail.com>
2025-08-02 21:20:21 -0300

    modificados para a versão compatível 8.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("com.android.library") version "8.11.1" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("com.android.library") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
-    id("com.github.ben-manes.versions") version "0.52.0" apply true
 }
 
 allprojects {
@@ -16,14 +14,8 @@ allprojects {
     }
 }
 
-fun isNonStable(candidate: ModuleComponentIdentifier): Boolean {
-    return listOf("alpha", "beta", "rc", "snapshot").any { keyword ->
-        keyword in candidate.version.lowercase()
-    }
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate)
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
     }
 }
