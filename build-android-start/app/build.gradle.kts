c6cc35ffc3bd9b7174fee276d089159addd36174
Wilson <wilsonfreitashgem@gmail.com>
2025-08-12 02:40:56 -0300

    mostram todas as etapas que o Gradle executou, como juntar recursos, compilar o código Kotlin e Java. e empacotar o aplicativo. Agora é rodar no Emulador ou em um dispositivo físico.

--- build-android-start/app/build.gradle.kts
+++ build-android-start/app/build.gradle.kts
@@ -54,6 +54,9 @@ android {
 }
 
 dependencies {
+    // Dependência do CircleImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
